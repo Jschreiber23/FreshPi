19b89b08bc8dda3b4fee45d8fa6b3f3384fb8c24
Jonathan Schreiber <Jschreiber11@hotmail.com>
2025-05-05 13:06:27 -0700

    Updated setup.sh removed Py3 install command

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 sudo apt update 
 sudo apt upgrade --y 
-sudo apt install -y git curl build-essential python3 python3-pip 
 sudo apt install -y code 
 sudo apt install -y realvnc-vnc-server
 echo "FreshPi Dev setup conmplete!" 
