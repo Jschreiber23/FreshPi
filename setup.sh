b7d9f849a59b15aa4c95ada3a1af78d92ce25922
Jonathan Schreiber <Jschreiber11@hotmail.com>
2025-05-05 13:00:50 -0700

    Updated setup.sh with more install commands

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 sudo apt update 
 sudo apt upgrade --y 
 sudo apt install -y git curl build-essential python3 python3-pip 
-echo "Dev setup conmplete!" 
+sudo apt install -y code 
+sudo apt install -y realvnc-vnc-server
+echo "FreshPi Dev setup conmplete!" 
